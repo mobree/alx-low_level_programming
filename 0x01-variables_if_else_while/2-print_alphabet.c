91d0526499186f1424ff0adc2dec4a1b7893b91f
mobree <elbakrimoha@gmail.com>
2023-06-18 09:53:31 -0700

    print alphabetic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (success)
+ */
+
+int main(void)
+
+{
+	char c;
+       
+	for(c = 'A'; c <= 'Z'; c++)
+	{
+        putchar(c);
+	putchar('\n');
+	}
+	return 0;
+
+}
