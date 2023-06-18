1fc2d6389e4bd083de18fb97217cb6bb78514fa8
mobree <elbakrimoha@gmail.com>
2023-06-18 10:06:25 -0700

    print alphabetic

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@ int main(void)
 
 {
 	char c;
-       
-	for(c = 'A'; c <= 'Z'; c++)
+
+	for (c = 'A'; c <= 'Z'; c++)
 	{
-        putchar(c);
+	putchar(c);
 	putchar('\n');
 	}
-	return 0;
+	return (0);
 
 }
